4917c9686f913a30fcf956728d6b26a8c501f025
Hamberfim <adhamlingithub@gmail.com>
2023-12-23 12:24:29 -0600

    chapter 3 data limits

--- dataTypeLimits/main.cpp
+++ dataTypeLimits/main.cpp
@@ -13,4 +13,30 @@ int main()
     cout << "Short ranges from " << SHRT_MIN;
     cout << " up to " << SHRT_MAX;
     cout << endl << endl;
+
+    cout << "Unsigned short ranges up to " << USHRT_MAX;
+    cout << endl << endl;
+
+    cout << "Int ranges from " << INT_MAX;
+    cout << " up to " << INT_MAX;
+    cout << endl << endl;
+
+    cout << "Unsigned int ranges up to " << UINT_MAX;
+    cout << endl << endl;
+
+    cout << "Long int ranges from " << LONG_MIN;
+    cout << " up to " << LONG_MAX;
+    cout << endl << endl;
+
+    cout << "Unsigned long int ranges up to " << ULONG_MAX;
+    cout << endl << endl;
+
+    cout << "Long long int ranges from " << LLONG_MIN;
+    cout << " up to " << LLONG_MAX;
+    cout << endl << endl;
+
+    cout << "Unsigned long long int ranges up to " << ULLONG_MAX;
+    cout << endl;
+
+    return 0;
 }
