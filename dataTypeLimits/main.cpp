77b3caf17eda074228168bea5bce49d506744fd1
Hamberfim <adhamlingithub@gmail.com>
2023-12-23 12:15:22 -0600

    chapter 3 data limits

---
+++ dataTypeLimits/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+    cout << "Char ranges from " << CHAR_MIN;
+    cout << " up to " << CHAR_MAX;
+    cout << endl << endl;
+
+    cout << "Short ranges from " << SHRT_MIN;
+    cout << " up to " << SHRT_MAX;
+    cout << endl << endl;
+}
